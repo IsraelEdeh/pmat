cf9b2be630629ff8a67fcc0cdab8661f411b15c5
Edeh <iedeh@deloitte.com.ng>
2025-07-05 14:06:15 +0100

    landing page

--- app/(dashboard)/dashboard/portfolio/create/page.tsx
+++ app/(dashboard)/dashboard/portfolio/create/page.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
 
 const page = () => {
+  const [currentStep, setCurrentStep] = useState(1);
   return (
-    <div>page</div>
-  )
-}
+    <div className="mt-6">
+      <div className="flex items-center justify-center mb-[18px]">
+        <div className="flex items-center">
+          <button
+            onClick={() => setCurrentStep(1)}
+            className={`${
+              currentStep === 1
+                ? "border border-primary text-[#041A57]"
+                : "bg-white border border-[#E9EAEC]"
+            } py-[10px] px-[12px] rounded-[8px] flex items-center space-x-2 w-fit`}
+          >
+            <span
+              className={`${
+                currentStep === 1 ? "border border-[#5A84E2] " : ""
+              }  rounded-[4px] text-xs py-[1px] px-[7px]`}
+            >
+              1
+            </span>
+            <span className="text-sm">Basic Details</span>
+          </button>
+          <div
+            className={`${
+              currentStep === 2 ? "bg-primary" : "bg-[#E9EAEC]"
+            } w-[80px] border `}
+          ></div>
+          <button
+            onClick={() => setCurrentStep(2)}
+            className={`${
+              currentStep === 2
+                ? "border border-primary text-[#041A57]"
+                : "bg-white border border-[#E9EAEC] text-[#808891]"
+            } py-[10px] px-[12px] rounded-[8px] flex items-center space-x-2 w-fit`}
+          >
+            <span
+              className={`${
+                currentStep === 2 ? "border border-[#5A84E2] " : "border border-[#A9AFB9]"
+              }  rounded-[4px] text-xs py-[1px] px-[7px]`}
+            >
+              2
+            </span>
+            <span className="text-sm">Location & Personnel</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- components/widgets/dashboard/DbHeader.tsx
+++ components/widgets/dashboard/DbHeader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { BellIcon, ChatBubbleLeftRightIcon } from "@heroicons/react/20/solid";
-import { BellAlertIcon } from "@heroicons/react/24/outline";
+import { BellAlertIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
 import React from "react";
 import { usePathname } from "next/navigation";
 
@@ -9,8 +9,12 @@ const getHeaderTitle = (pathname: string) => {
   switch (true) {
     case pathname === "/dashboard":
       return "Hello Victor";
-    case pathname.includes("portfolio"):
+    case pathname === "/dashboard/portfolio":
       return "My Portfolio";
+      case pathname === "/dashboard/portfolio/":
+      return "My Portfolio";
+    case pathname === "/dashboard/portfolio/create":
+      return "Create Property";
     case pathname.includes("contacts"):
       return "Contacts";
     default:
@@ -22,8 +26,10 @@ const getHeaderSubtitle = (pathname: string) => {
   switch (true) {
     case pathname === "/dashboard":
       return "Overview of your portfolio's performance & tasks";
-    case pathname.includes("portfolio"):
+    case pathname === "/dashboard/portfolio":
       return "List of all your properties";
+    case pathname === "/dashboard/portfolio/create":
+      return "Overview of your portfolio’s performance & tasks";
     case pathname.includes("contacts"):
       return "Manage your tenants and technicians";
     default:
@@ -38,9 +44,15 @@ const DbHeader = () => {
 
   return (
     <nav className="border-b border-b-[#E9EAEC] py-[19px] px-6 flex items-center justify-between">
-      <div>
-        <h2 className="text-primary text-[20px] font-bold">{title}</h2>
-        <p className="text-sm font-medium">{subtitle}</p>
+      <div className="flex items-center space-x-4">
+        {pathname !== "/dashboard" && (
+          <ChevronLeftIcon className="w-6 h-6 text-[#808891] cursor-pointer hover:text-primary/90 transition-colors" />
+        )}
+
+        <div>
+          <h2 className="text-primary text-[20px] font-bold">{title}</h2>
+          <p className="text-sm font-medium text-[#808891]">{subtitle}</p>
+        </div>
       </div>
 
       <div className="flex items-center space-x-4">
